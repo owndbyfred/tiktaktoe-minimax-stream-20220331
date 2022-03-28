a888c25c4aecba194bd10df7bdae19b40bf5ba43
Frederik Diekmann <frediek@gmx.de>
2022-03-28 19:21:08 +0200

    Implement checking for winner

--- src/logic/Board.js
+++ src/logic/Board.js
@@ -24,7 +24,9 @@ export default class Board {
       tileEle.addEventListener("click", ({ target }) => {
         const coordinates = target.getAttribute("data-index").split("-");
 
-        this.makeTurn(coordinates[0], coordinates[1]);
+        if (!this.makeTurn(coordinates[0], coordinates[1])) {
+          alert("Bitte wähle ein anderes Feld!");
+        }
       });
     });
   }
@@ -45,6 +47,12 @@ export default class Board {
     // Next player's turn
     this.playerTurn = this.playerTurn === PLAYER_1 ? PLAYER_2 : PLAYER_1;
 
+    // Check if winner:
+    const winner = this.checkWinner();
+    if (winner) {
+      alert(`Spieler ${winner} hat gewonnen!`);
+    }
+
     return true;
   }
 
@@ -56,5 +64,92 @@ export default class Board {
     return Array.from(this.tileEles).find(
       (tileEle) => tileEle.getAttribute("data-index") === `${x}-${y}`
     );
+  }
+
+  checkWinner() {
+    for (let i = 0; i < this.state.length; i++) {
+      for (let j = 0; j < this.state[i].length; j++) {
+        const playerOnTile = this.state[i][j];
+
+        if (playerOnTile === TILE_EMPTY) continue;
+
+        let success = true;
+
+        // Check horizontal line
+        this.state[i].forEach((tile) => {
+          if (tile !== playerOnTile) {
+            success = false;
+            return;
+          }
+        });
+
+        if (success) {
+          return playerOnTile;
+        }
+
+        success = true;
+
+        // Check vertical line
+        for (let k = 0; k < this.state.length; k++) {
+          if (this.state[k][j] !== playerOnTile) {
+            success = false;
+            break;
+          }
+        }
+
+        if (success) {
+          return playerOnTile;
+        }
+      }
+    }
+
+    // Check diagonals
+    /*
+        1 0 0 
+        0 1 0
+        0 0 1
+    */
+
+    let playerOnTile = this.state[0][0];
+    let success = true;
+
+    if (playerOnTile !== TILE_EMPTY) {
+      if (
+        !(
+          this.state[1][1] === playerOnTile && this.state[2][2] === playerOnTile
+        )
+      ) {
+        success = false;
+      }
+    }
+
+    if (success) {
+      return playerOnTile;
+    }
+
+    /*
+        0 0 1 
+        0 1 0
+        1 0 0
+    */
+
+    playerOnTile = this.state[0][2];
+    success = true;
+
+    if (playerOnTile !== TILE_EMPTY) {
+      if (
+        !(
+          this.state[1][1] === playerOnTile && this.state[2][0] === playerOnTile
+        )
+      ) {
+        success = false;
+      }
+    }
+
+    if (success) {
+      return playerOnTile;
+    }
+
+    return null;
   }
 }
